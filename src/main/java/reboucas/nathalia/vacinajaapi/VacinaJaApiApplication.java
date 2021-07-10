3f260d2225e5145de5fbd23f224481acf3a376e0
nathaliareboucas <nathreboucas5@gmail.com>
2021-07-10 16:46:36 -0300

    first commit

---
+++ src/main/java/reboucas/nathalia/vacinajaapi/VacinaJaApiApplication.java
@@ -0,0 +1,13 @@
+package reboucas.nathalia.vacinajaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinaJaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinaJaApiApplication.class, args);
+	}
+
+}
