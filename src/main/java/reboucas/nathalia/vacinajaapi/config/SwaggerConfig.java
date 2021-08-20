67665cbb28b83d7a865d41cb6f9688123504ef9c
nathaliareboucas <nathreboucas5@gmail.com>
2021-08-20 00:46:22 -0300

    customizando swagger

--- src/main/java/reboucas/nathalia/vacinajaapi/config/SwaggerConfig.java
+++ src/main/java/reboucas/nathalia/vacinajaapi/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package reboucas.nathalia.vacinajaapi.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,9 +21,22 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("reboucas.nathalia.vacinajaapi"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(apiInfo());
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfo(
+				"Vacina Já", 
+				"API do sistema de cadasto para vacinação COVID-19", 
+				"v1", 
+				"Termos de serviço: ----", 
+				new Contact("Nathália Rebouças", "github.com/nathaliareboucas", "nathreboucas5@gmail.com"),
+				"Licença: MIT", 
+				"URL Licança: ----", 
+				Collections.emptyList());
 	}
 
 }
